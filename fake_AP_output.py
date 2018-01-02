98da309c177acde90107156ca5cec6e028474eeb
kiki0805 <kiki_hzz@163.com>
2018-01-02 23:03:07 +0800

    add arrays

--- fake_AP_output.py
+++ fake_AP_output.py
@@ -2,19 +2,42 @@ import time
 import random  
 import string  
 
+associated = []
+disassociated = []
+
 while 1:
-	chr_num = ''  
-	for i in range(12):  
-	    str_num = str(random.choice('abcdef' + string.digits))  
-	    chr_num += str_num  
-	
-	fake_MAC = ''
-	for chr in chr_num:
-		if len(fake_MAC) in [2, 5, 8, 11, 14]:
-			fake_MAC += ':'
-		fake_MAC += chr
-	choice1 = 'wlan0: STA %s IEEE 802.11: associated' % fake_MAC
-	choice2 = 'wlan0: STA %s IEEE 802.11: disassociated' % fake_MAC
-	choice3 = 'Something else(ignored)'
-	print(random.choice([choice1, choice2, choice3]))
-	time.sleep(random.randint(1,3))
+    chr_num = ''  
+    for i in range(12):  
+        str_num = str(random.choice('abcdef' + string.digits))  
+        chr_num += str_num  
+
+    fake_MAC = ''
+    for char in chr_num:
+        if len(fake_MAC) in [2, 5, 8, 11, 14]:
+            fake_MAC += ':'
+        fake_MAC += char
+
+    choices = []
+    choice1 = 'wlan0: STA %s IEEE 802.11: associated' % fake_MAC
+    choices.append(choice1)
+    choice2_MAC = ''
+    choice2 = ''
+    if associated != []:
+        choice2_MAC = random.choice(associated)
+        choice2 = 'wlan0: STA %s IEEE 802.11: disassociated' % choice2_MAC
+        choices.append(choice2)
+    choices.append('Something else(ignored)')
+    random_choice = random.choice(choices)
+    print(random_choice)
+
+    if random_choice == choice1:
+        associated.append(fake_MAC)
+    elif random_choice == choice2:
+        associated.remove(choice2_MAC)
+        disassociated.append(choice2_MAC)
+    else:
+        pass
+
+    time.sleep(random.randint(1,3))
+
+
