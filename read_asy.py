cbd1f3ea724dd2cda5f8fc2407b29e36eb06c07a
kiki0805 <kiki_hzz@163.com>
2018-01-04 19:05:32 +0800

    fix

--- read_asy.py
+++ read_asy.py
@@ -1,20 +1,16 @@
 from subprocess import Popen, PIPE, CalledProcessError
-import threading
 
-connec
 
-def execute():
-    cmd = ' '.join(["python fake_AP_output.py"])
+def execute(cmd):
     proc = Popen(cmd, shell=True, stdout=PIPE, bufsize=1,universal_newlines=True)
     while True:
         line = proc.stdout.readline()
         if not line:
             break
-        handle_line(line)
         print(line, end='')
     proc.wait()
     if proc.returncode != 0:
         raise CalledProcessError(p.returncode, p.args)
+cmd = ' '.join(["python fake_AP_output.py"])
 
-t = threading.Thread(target=execute)
-t.start()
+execute(cmd)
